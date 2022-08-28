7761256a24294765e498d8f61c193e6cbf88ed11
Metisek <matboj5@gmail.com>
2022-08-28 22:06:30 +0200

    Arduino code update

--- Electronics/Arduino_code/Arduino_code.ino
+++ Electronics/Arduino_code/Arduino_code.ino
@@ -1,8 +1,8 @@
-//Dane do zmiany
-char CHARACTER = 59 ;  //Separator w łańcuchu znaków dla liczb (59 ASCII = ;)
+// ASCII variable to change
+char CHARACTER = 59 ;  // 59 ASCII = ;
 
 #include <ShiftRegister74HC595.h>
-ShiftRegister74HC595<8> sr(2, 3, 4);  // Piny arduino, kolejno dla SDO, SCLK i LOAD
+ShiftRegister74HC595<8> sr(2, 3, 4);  // SDO, SCLK and LOAD pins in order
 
 uint8_t  nB[] = {B11000000, //0
                       B11111001, //1 
@@ -17,11 +17,11 @@ uint8_t  nB[] = {B11000000, //0
                      };
 
 String serialData;
-uint8_t registerNumbers[8] = {nB[0], nB[1], nB[2], nB[3], nB[4], nB[5], nB[6], nB[7]}; // Kolejno od 0 do 7, można pozmieniać indeksy
+uint8_t registerNumbers[8] = {nB[0], nB[1], nB[2], nB[3], nB[4], nB[5], nB[6], nB[7]}; // Set numbers form 0 to 7 in order
 
 uint8_t tempNumbers[4] = {0,0,0,0};
 
-String getValue(String data, char separator, int index){ // Zwraca dany kawałek teksty oddzielony znakiem (np.: ";")
+String getValue(String data, char separator, int index){ // Return part of string using CHARACTER as separator
   int found = 0;
   int strIndex[] = {0, -1};
   int maxIndex = data.length() -1;
@@ -36,7 +36,7 @@ String getValue(String data, char separator, int index){ // Zwraca dany kawałek
   return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
-unsigned int getMaxSeparators(String data, char separator){ // Zwraca ilość wystąpień danego separatora
+unsigned int getMaxSeparators(String data, char separator){ // Counts hom much CHARACTERs are in string
   int found = 0;
   int maxIndex = data.length() -1;
 
@@ -69,7 +69,7 @@ void loop() {
     bool check = 1;
 
     if (nrOfIndexes != 3){
-      Serial.println(String("Error: podano następującą ilość rozdzieleń znaku: " + String(nrOfIndexes) + " dla znaku: '" + String(CHARACTER) +"', ciąg powinien posiadać 3."));
+      Serial.println(String("Error: found: " + String(nrOfIndexes) + " '" + String(CHARACTER) +"' characters, 3 are required in string."));
       check = 0;
     } else {
       
@@ -78,7 +78,7 @@ void loop() {
         int numer = sub_serialData.toInt();
       
         if (numer < 0 || numer > 99){
-          Serial.println("Error: liczba " + String(numer) + " w złym zakresie (należeć ma do zakresu od 0 do 99)");
+          Serial.println("Error: Number " + String(numer) + " is not in rage from 0 to 99");
           check = 0;
           break;
         } else {
